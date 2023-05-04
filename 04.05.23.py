c36535b4f9eda6873492e3350249bf85485849b5
student <maksimkozlovskiy2011@gmail.com>
2023-05-04 19:20:59 +0300

    підрахунок екземплярів класів

--- 04.05.23.py
+++ 04.05.23.py
@@ -3,9 +3,11 @@
 class Student:
     print("Hi!")
     count = 0
-    def __init__(self , height = 150):
+    def __init__(self , height = 150): #конструктор класу
         self.height = height
+        Student.count += 1
 dima = Student() #об'єкт , екземпляр класу
 print(dima.height)
 masha = Student(height = 200)
 print(masha.height)
+print(Student.count)
