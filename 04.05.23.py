8e58efba60ea69648a7ea1fd8dc13d82ebad3643
student <maksimkozlovskiy2011@gmail.com>
2023-05-04 19:15:44 +0300

    перший приклад атрибутів класу

--- 04.05.23.py
+++ 04.05.23.py
@@ -1,1 +1,11 @@
+#Класи
 
+class Student:
+    print("Hi!")
+    count = 0
+    def __init__(self , height = 150):
+        self.height = height
+dima = Student() #об'єкт , екземпляр класу
+print(dima.height)
+masha = Student(height = 200)
+print(masha.height)
