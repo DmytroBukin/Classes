b5ed7e6611c44e15448424d87aed39f1eb388545
student <maksimkozlovskiy2011@gmail.com>
2023-05-04 19:49:34 +0300

    методи класів

--- 04.05.23.py
+++ 04.05.23.py
@@ -6,8 +6,11 @@ class Student:
     def __init__(self , height = 150): #конструктор класу
         self.height = height
         Student.count += 1
+    def breathing(self): #методи класів
+        return self.height - 10
 dima = Student() #об'єкт , екземпляр класу
 print(dima.height)
 masha = Student(height = 200)
 print(masha.height)
 print(Student.count)
+print(masha.breathing())
